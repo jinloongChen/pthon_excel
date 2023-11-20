f49de93b2bcd8fe446c467483c8d6db2ea5117d5
ChenJl15 <3067975098@qq.com>
2023-11-20 10:10:51 +0800

    master first commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+
+	printf("hello,world!");
+	printf("hello,world!");
+	printf("hello,world!");
+	printf("hello,world!");
+	printf("hello,world!");
+	printf("hello,world!");
+	printf("hello,dev");
+	printf("hello,world!");
+	printf("hello,master!");
+	printf("hello,world!");
+
+
+
+}
